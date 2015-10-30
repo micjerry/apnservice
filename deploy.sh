70936d2350af412e6eab2866e44f0fa6df1ad351
micjerry <hansfei@126.com>
2015-10-30 09:32:14 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#the name of app
+APP_NAME="app_apns"
+
+#the github path of app
+APP_GIT_PATH="https://github.com/micjerry/apnservice.git"
+
+#the root path of app
+ROOT_PATH="/opt/webapps"
+APP_PATH="${ROOT_PATH}/${APP_NAME}"
+START_PORT=19000
+
+CONF_ROOT_PATH="/etc/mx_apps"
+APP_CONF_PATH="${CONF_ROOT_PATH}/${APP_NAME}"
+PORT_FLAG="{port}"
+INSTANCE_FLAG="{instance}"
+
+AUTO_START_SC="apns-server"
+
+TMP_GIT_PATH="/opt/tempgit"
+
+. /etc/init.d/deploy_apps
+
